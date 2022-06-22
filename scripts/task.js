56d39cf5f39bd05c5d54ff4b94b3b1324b3a74ea
Sychova Ksenia <mlichlundiapl@gmail.com>
2022-06-22 10:00:09 +0200

    US004: change double to single quotes, add named funtion instead arrow

--- scripts/task.js
+++ scripts/task.js
@@ -1,7 +1,7 @@
-import { Check } from "./check.js";
-import { formatDate } from "./date.js";
-import { tomorrow } from "./date.js";
-import { generateId } from "./generateId.js";
+import { Check } from './check.js';
+import { formatDate } from './date.js';
+import { tomorrow } from './date.js';
+import { generateId } from './generateId.js';
 
 export class Task {
 	taskContainer;
@@ -34,18 +34,18 @@ export class Task {
 	}
 
 	init() {
-		this.taskContainer = document.createElement("li");
-		this.taskInfo = document.createElement("div");
-		this.controlContainer = document.createElement("div");
-		this.task = document.createElement("h3");
-		this.term = document.createElement("p");
-		this.deleteButton = document.createElement("button");
+		this.taskContainer = document.createElement('li');
+		this.taskInfo = document.createElement('div');
+		this.controlContainer = document.createElement('div');
+		this.task = document.createElement('h3');
+		this.term = document.createElement('p');
+		this.deleteButton = document.createElement('button');
 	}
 
 	createElement() {
 		this.task.innerText = this.taskText;
 		this.term.innerHTML = `${this.start} — ${this.deadline}`;
-		this.deleteButton.innerText = "✖";
+		this.deleteButton.innerText = '✖';
 
 		this.taskInfo.append(this.task);
 		this.taskInfo.append(this.term);
@@ -60,33 +60,33 @@ export class Task {
 	}
 
 	setClasses() {
-		this.taskContainer.classList.add("task__container");
-		this.taskInfo.classList.add("task__info");
-		this.task.classList.add("task");
-		this.deleteButton.classList.add("task__button");
+		this.taskContainer.classList.add('task__container');
+		this.taskInfo.classList.add('task__info');
+		this.task.classList.add('task');
+		this.deleteButton.classList.add('task__button');
 	}
 
 	setAttributes() {
-		this.taskContainer.setAttribute("id", this.id);
+		this.taskContainer.setAttribute('id', this.id);
 	}
 
 	setHendlers() {
-		this.check = this.controlContainer.querySelector("input");
-		this.check.addEventListener("change", () => this.toggleDone());
-		this.deleteButton.addEventListener("click", () => this.delete());
+		this.check = this.controlContainer.querySelector('input');
+		this.check.addEventListener('change', this.toggleDone.bind(this));
+		this.deleteButton.addEventListener('click', this.delete.bind(this));
 	}
 
 	toggleDone() {
-		const state = this.taskContainer.getAttribute("data-status");
+		const state = this.taskContainer.getAttribute('data-status');
 
-		if (state === "done") {
-			this.taskContainer.removeAttribute("data-status", "done");
-			this.task.style.textDecoration = "";
-			this.task.style.color = "";
+		if (state === 'done') {
+			this.taskContainer.removeAttribute('data-status', 'done');
+			this.task.style.textDecoration = '';
+			this.task.style.color = '';
 		} else {
-			this.taskContainer.setAttribute("data-status", "done");
-			this.task.style.textDecoration = "line-through";
-			this.task.style.color = "rgb(98 102 116)";
+			this.taskContainer.setAttribute('data-status', 'done');
+			this.task.style.textDecoration = 'line-through';
+			this.task.style.color = 'rgb(98 102 116)';
 		}
 	}
 
